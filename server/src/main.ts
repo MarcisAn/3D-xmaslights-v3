45ef7336e057335cb1619da72d0db3209dd2b262
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2023-12-05 09:44:25 +0200

    server cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
   origin: [
-    'http://lampinas.vercel.app',
+    '*',
   ],
-  credentials: false,
 });
   await app.listen(3000);
 }
