5077b3d70b301c404e1a08baf17a27576cf009f5
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2023-12-10 11:54:37 +0200

    format cords

--- csv_to_json.py
+++ csv_to_json.py
@@ -7,7 +7,7 @@ cords = []
 
 index = 0
 for i in f.read().split('\n'):
-    cords.append({"x": i.split(",")[0], "y": i.split(",")[1],"z": i.split(",")[2]})
+    cords.append({"x": int(i.split(",")[0]), "y": int(i.split(",")[1]),"z": int(i.split(",")[2])})
     #requests.post("http://localhost:3000/updateCords", json= {'index': index, 'cords': i})
     #time.sleep(0.4)# pieprasījumi atjaunināt koordinātas jāsūta lēnām, lai 'prisma' tur līdzi
     #print(i)
