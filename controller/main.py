9c10960bc9b1772ee6963b1099cf8374403e9f64
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2023-12-11 00:05:37 +0200

    add neopixel

--- controller/main.py
+++ controller/main.py
@@ -1,10 +1,18 @@
 import math
+import random
 import time
 
+import requests
 import socketio
 import json
 import time
 
+try:
+    import board
+    import neopixel
+    pixels = neopixel.NeoPixel(board.D18, 200, brightness=1)
+except:
+    print("neopixel error")
 # standard Python
 sio = socketio.Client()
 
@@ -12,22 +20,53 @@ anim_frame_delay_time = 1.0
 anim_frame_index = 0
 anim_frames = []
 
+time_anim_started = time.time()
+
 @sio.on('reciveAnimation')
 def on_message(data):
     global anim_frame_delay_time
     anim_frame_delay_time = math.pow(int(data['speed']), -1)
     global anim_frames
     anim_frames = data['animation']
+    global anim_frame_index
+    anim_frame_index = 0
 
+def run_new_anim_from_cache():
+    cache = requests.get("https://lampinas-server.cvgmerch.lv/getAnimCache")
+    len_cache = len(cache.json())
+    selected_anim = cache.json()[random.randint(0, len_cache-1)]
+    global anim_frames
+    anim_frames = selected_anim['data']
+    global anim_frame_index
+    anim_frame_index = 0
+    global anim_frame_delay_time
+    anim_frame_delay_time = math.pow(int(selected_anim['speed']), -1)
+    global time_anim_started
+    time_anim_started = time.time()
 
 
 sio.connect("wss://lampinas-server.cvgmerch.lv", transports=['websocket'])
 print('connection ready, sio: ', sio.sid)
 
+run_new_anim_from_cache()
+
 while True:
+    if time.time() - time_anim_started > 12:
+        run_new_anim_from_cache()
     if len(anim_frames) == 0:
         continue
-    print(ord(anim_frames[anim_frame_index][1]))
+
+    frame = anim_frames[anim_frame_index]
+
+    light = ''
+    loop_index = 1
+    for i in frame:
+        light = light + i
+        if loop_index % 4 == 0:
+            loop_index = 1
+            pixels[ord(light[0])] = (ord(light[1]), ord(light[2]),ord(light[3]))
+
+    pixels.show()
     print(anim_frame_index,anim_frames[anim_frame_index])
     if anim_frame_index == len(anim_frames)-1:
         anim_frame_index = 0
